a828be759e458187d4339526488783527f39b7ad
autulin <autulin@outlook.com>
2017-12-26 21:19:53 +0800

    1.通过babel-plugin-component减小体积 2.修改build的static的路径为相对路径

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+import { Button, Select, Input, Option } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+Vue.component(Input.name, Input)
+Vue.component(Option.name, Option)
 
 /* eslint-disable no-new */
 new Vue({
